361dea30b3ec98699eaddcf5e4a92b53eea6db47
pagdas95 <pantelisa1995@gmail.com>
2024-10-25 23:29:28 +0300

    new changes to the name verto

--- nfc_showcase/asgi.py
+++ nfc_showcase/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for nfc_showcase project.
+ASGI config for verto project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nfc_showcase.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'verto.settings')
 
 application = get_asgi_application()
